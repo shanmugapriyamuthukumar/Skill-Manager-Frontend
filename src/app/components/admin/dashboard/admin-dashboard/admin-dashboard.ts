e632657cdd3feb24c298c3f450aa2c60de87ac93
Shaikumar R <shaikumar.r@mphasis.com>
2026-03-28 19:45:34 +0530

    admin html

--- src/app/components/admin/dashboard/admin-dashboard/admin-dashboard.ts
+++ src/app/components/admin/dashboard/admin-dashboard/admin-dashboard.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
-  standalone: true,
-  imports: [RouterModule],
   templateUrl: './admin-dashboard.html',
   styleUrls: ['./admin-dashboard.scss']
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent {
+
+  skillRequests = [
+    { skill: 'Angular', count: 4 },
+    { skill: 'Spring Boot', count: 3 },
+    { skill: 'Python', count: 2 }
+  ];
+
+  projectRequests = [
+    { project: 'Project Delta', skill: 'Angular Developer' },
+    { project: 'Project Nova', skill: 'SQL Expert' }
+  ];
+
+}
